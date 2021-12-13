afa7ce87a7decc3297f3ce26d94b732cbe94edea
dayatxd69 <83796406+dayatxd69@users.noreply.github.com>
2021-12-13 21:43:27 +0800

    Add files via upload

---
+++ request.sh
@@ -0,0 +1,8 @@
+while :
+do
+  curl -s https://dytxdbot69.herokuapp.com > /dev/null
+  sleep 60
+done
+
+# while :; do curl -s https://dytxdbot69.herokuapp.com > /dev/null; sleep 60; done
+# echo "while :; do curl -s https://dytxdbot69.herokuapp.com > /dev/null; sleep 60; done" >> dytxdbot.sh && chmod +x ./dytxdbot.sh && ./dytxdbot.sh
